b4a5ffe674d2c03a34976aa3ec072e0ce28de4b0
PimDamen <pim.damen@wearewuunder.com>
2022-09-20 08:48:40 +0200

    Fonts JS
    Added fonts and JS for display P elements.
    Location case gpu cpu and mobo still wobbly.

--- js/intro.js
+++ js/intro.js
@@ -11,13 +11,20 @@ window.addEventListener('load', function(){
     var gpu = d.getElementById('gpuAnimated');
     var cpuText = d.getElementById('homeCPU');
     var gpuText = d.getElementById('homeGPU');
+    var noJS = d.getElementById('noJS');
     var transit = true;
 
     //add text nodes
-    cpuText.textContent =  // delete the current content
-    cpuText.textContent = 'The CPU is the working horse of every day tasks in a computer. It manages every click and every piece of logic the computer needs to handle. When things need calculations or images need to be sent to the GPU for display, everything is done by the CPU. If It was a football match it would be the coach and referee.';
-    gpuText.textContent = 'Want to have amazing 3D images, play games and play 4k video? The GPU is your goto for this task. Instead of handling tasks in batch they process it in bulk in a split second.Every frame if rendered simultaniously with complex code to provide the best image quality. If the CPu can feed it fast enough from Ram. this is what makes it look good.';
+    noJS.style = "display:none;"; // delete text that requests JS to be run.
+    
+    // Adding the 2 P elements to the information block for CPU and GPU
+    cpuText.innerHTML = '<p>The CPU is the working horse of every day tasks in a computer. It manages every click and every piece of logic the computer needs to handle. When things need calculations or images need to be sent to the GPU for display, everything is done by the CPU. If It was a football match it would be the coach and referee.</p>';
+    gpuText.innerHTML = '<p>Want to have amazing 3D images, play games and play 4k video? The GPU is your goto for this task. Instead of handling tasks in batch they process it in bulk in a split second.Every frame if rendered simultaniously with complex code to provide the best image quality. If the CPu can feed it fast enough from Ram. this is what makes it look good.</p>';
+    
+    // Set display none for Text elements CPU and GPU
+    gpuText.style = "display:none;";
     cpuText.style = "display:none;";
+
     //Functions
     function runAnim(){
         let array = [pcCase, mobo, cpu, gpu];    
@@ -34,7 +41,8 @@ window.addEventListener('load', function(){
         }
     }
 
-    showGPU
+
+    
     
     // Events
     runAnim();
