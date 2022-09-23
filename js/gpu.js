e93e1c62153658c44a842fbf77f8379b5bca4fa2
PimDamen <pim.damen@wearewuunder.com>
2022-09-23 14:09:51 +0200

    working tekst
    test working, style not just yet

--- js/gpu.js
+++ js/gpu.js
@@ -1,52 +1,47 @@
 var d = document;
 window.addEventListener('load', function(){
-    console.log("started");
+    
     if(!d.getElementById("gpuImage"))return false;
 
     // Existency check
 
     // image and part of image
-    gpuImage = d.getElementById('gpuImage');
-    gpuChips = d.getElementById('gpuChips');
-    gpuPowersupply = d.getElementById('gpuPowersupply');
-    gpuFans = d.getElementById('gpuFan');
-    gpuPCIE = d.getElementById('gpuPCIE');
-    gpuCooler = d.getElementById('gpuCooler');
-    //    other 
-    gpuInfoContainer = d.getElementById('gpuInformation')
+    // gpuImage = d.getElementById('gpuImage');
+    // gpuChips = d.getElementById('gpuChips');
+    // gpuPowersupply = d.getElementById('gpuPowersupply');
+    // gpuFans = d.getElementById('gpuFan');
+    // gpuPCIE = d.getElementById('gpuPCIE');
+    // gpuCooler = d.getElementById('gpuCooler');
+    // //    other 
+    // gpuInfoContainer = d.getElementById('gpuInformation')
     gpuInfo = d.getElementById('gpuInfoText');
-    // test
-
+   
     // Constructs query for all children of the map tag
     const mapItems = d.querySelector('map').children;
     // construct put all queried results of the map tag into an array called mapArray
     const mapArray = Array.from(mapItems);
 
-    // console.log(mapArray);
+   
     // Functions
-    // for(area of mapArray){
-        for(i=0; i <= 4; i++){
-        // console.log(mapArray[i].id); // looks for all id's within the current loop. 
+        // make a loop of all the mapArray parts
+        for(i=0; i <= mapArray.length; i++){
         // area is now per loop defined by the ID
-        var area = d.getElementById(mapArray[i].id);
-        // add events per area  
-        console.log(tekstenGpu[i]);   
-        area.addEventListener('mouseover', function(){
-            gpuInfo.textContent = tekstenGpu[i].txt;         
+        mapArrayItem = mapArray[i];
+        var area = d.getElementById(mapArrayItem.id); // gives an error but the code works and id does exist if you print it in the console, so keep it!
+        // add events per area
+        area.style= ('border:10px; background-color: black; outline: 20px;');
+        console.log(area);
+        area.addEventListener('mouseover', function(e){
+            // console.log(e.target.id) 
+            // console.log(tekstenGpu[i].id);
+            for(tekst of tekstenGpu){ // loop through all tekst
+                if(e.target.id == tekst.id){ // find a matching tekst
+                    gpuInfo.textContent = tekst.txt; // put that tekst in the html tab for showing. 
+                }
+            };
+      
             
         });
     }
-    // } // map area for loop ends
-    // events
-    // d.addEventListener('mouseover', function(e){
-    //     var that = this.
-    //     console.log(that.element);
-    // });
-
-    
-
-
-
-
 
 });
