1469741e4fd9ad98ab6c52f4c389046e6f586c8d
PimDamen <pim.damen@wearewuunder.com>
2022-09-23 08:32:57 +0200

    .
    .

--- js/gpu.js
+++ js/gpu.js
@@ -1,6 +1,6 @@
 var d = document;
 window.addEventListener('load', function(){
-    $('img[usemap]').rwdImageMaps();
+    console.log("started");
     if(!d.getElementById("gpuImage"))return false;
 
     // Existency check
@@ -15,8 +15,29 @@ window.addEventListener('load', function(){
     //    other 
     gpuInfo = d.getElementById('gpuInformation')
     gpuInfo = d.getElementById('gpuInfoText');
+    // test
 
+    const mapItems = d.querySelector('map').children;
+    const mapArray = Array.from(mapItems);
 
+    console.log(mapArray);
+    // Functions
+    for(area of mapArray){
+        
+        area.addEventListener('mouseover', function(){
+            console.log(this.id);
+            let
+            
+            
+        });
+    }
+    // events
+    // d.addEventListener('mouseover', function(e){
+    //     var that = this.
+    //     console.log(that.element);
+    // });
+
+    
 
 
 
