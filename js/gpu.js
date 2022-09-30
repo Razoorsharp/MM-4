c07ca0c2e3ec0fbd9e15a3e930add3747b133efc
PimDamen <pim.damen@wearewuunder.com>
2022-09-30 15:28:33 +0200

    .
    .

--- js/gpu.js
+++ js/gpu.js
@@ -6,73 +6,70 @@ window.addEventListener('load', function(){
     // Existency check
 
     // image and part of image
-    gpuImage = d.getElementById('gpuImage');
-    // gpuChips = d.getElementById('gpuChips');
-    // gpuPowersupply = d.getElementById('gpuPowersupply');
-    // gpuFans = d.getElementById('gpuFan');
-    // gpuPCIE = d.getElementById('gpuPCIE');
-    // gpuCooler = d.getElementById('gpuCooler');
-    // //    other 
-    // gpuInfoContainer = d.getElementById('gpuInformation')
+    gpuImage = d.getElementById('gpuImage'); 
+    // Text Part of view   
     gpuInfo = d.getElementById('gpuInfoText');
+    // Image part of view
     gpuInfoImage = d.getElementById('gpuInfoImage');
 
-    // buttons
+    // Buttons container
     btnGpuChip = d.getElementById('btnGpuChip');
     
-   
+   // !! CONSTRUCTORS !! 
     // Constructs query for all children of the map tag
     const mapItems = d.querySelector('map').children;
     // construct put all queried results of the map tag into an array called mapArray
     const mapArray = Array.from(mapItems);
-
+    // Constructs query for all children of the buttons class
     const buttonItems = d.querySelector('.buttons').children;
-    // construct put all queried results of the map tag into an array called mapArray
+    // construct put all queried results of thebuttons into an array called buttonArray
     const buttonArray = Array.from(buttonItems);
-    // console.log(buttonArray);
+    
  
-    // Functions
+    // !! Functionality !!
         // make a loop of all the mapArray parts
-       
+        // !! note that .length starts at 1 and array starts at 0. so dont use <= !!        
         for(i=0; i < mapArray.length; i++){
-        // area is now per loop defined by the ID
+        // area is now per loop defined by the ID. 
         mapArrayItem = mapArray[i];
-        var area = d.getElementById(mapArrayItem.id); // gives an error but the code works and id does exist if you print it in the console, so keep it!
+        var area = d.getElementById(mapArrayItem.id); 
         // add events per area
         area.style= ('border:10px; background-color: black; outline: 20px;');
         // console.log(area);
         area.addEventListener('mouseover', function(e){
-            // console.log(e.target.id) 
-            // console.log(tekstenGpu[i].id);
-            for(tekst of tekstenGpu){ // loop through all tekst
-                if(e.target.id == tekst.id){ // find a matching tekst
-                    gpuInfo.textContent = tekst.txt; // put that tekst in the html tab for showing. 
+            for(tekst of tekstenGpu){ // loop through all text
+                if(e.target.id == tekst.id){ // find a matching text
+                    gpuInfo.textContent = tekst.txt; // put that text in the html tab for showing. 
                     gpuImage.src = "../images/main/"+ e.target.id +".png";
                     gpuInfoImage.innerHTML = "<img src=../images/GpuParts/"+ e.target.id +".png>";
                     console.log(gpuInfoImage);
-
                 }
             };
         });
         area.addEventListener('mouseout', function(e){
+            // revert to original image when no longer hovering over the areamap. 
             gpuImage.src = "../images/main/gpumain.png";
 
         });
-    
 }
 
     // button Loops. 
-    
-    
+    // make a loop of all the buttonArray parts and add eventlisteners
+    // !! note that .length starts at 1 and array starts at 0. so dont use <= !! 
     for(i=0; i < buttonArray.length; i++){
         console.log("started");
         buttonArrayItem = buttonArray[i];
-        var button = d.getElementById(buttonArrayItem.id); // gives an error but the code works and id does exist if you print it in the console, so keep it!
-        // add events per area
-        
+        var button = d.getElementById(buttonArrayItem.id); 
       
         button.addEventListener('click', function(e){
-            console.log(e.target.id);
+            for(tekst of tekstenGpu){ // loop through all text
+                if(e.target.name == tekst.id){ // find a matching text
+                    gpuInfo.textContent = tekst.txt; // put that text in the html tab for showing. 
+                    gpuImage.src = "../images/main/"+ e.target.name +".png";
+                    gpuInfoImage.innerHTML = "<img src=../images/GpuParts/"+ e.target.name +".png>";
+                    console.log(gpuInfoImage);
+                }
+            }
         });
     }
 });
